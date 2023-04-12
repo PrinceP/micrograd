48d83d001c56613b389e09cdf794ba9978f44af1
Prince Patel <prince.patel.14@icloud.com>
2023-04-12 20:34:01 +0530

    sample run

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # micrograd
 
-
 ## Test
 
+Tests individual micrograd func's one by one.
+
 `python3 -m pytest -v`
+
+## Test on MNIST
+
+`python3 train_test_onmicrograd.py`
+![training](test_run.jpg)
+
+The func's are HIGHLY unoptimized for the MNIST :W 
+
+## Experimenting and developing the micrograd
+
+The notebook `experiment.ipynb` follows the video line by line.
+
+## License
+
+MIT
--- train_test_onmicrograd.py
+++ train_test_onmicrograd.py
@@ -1,5 +1,8 @@
-import numpy as np
 import gzip
+import random
+
+from micrograd.multi_layer_perceptron import MLP
+
 
 # Load mnist
 def convert_to_np(imgf, labelf, n):
@@ -10,12 +13,11 @@ def convert_to_np(imgf, labelf, n):
     l.read(8)
     input_image_data = []
     input_label_data = []
-    
 
     for i in range(n):
         input_label_data.append(ord(l.read(1)))
         image = []
-        for j in range(28*28):
+        for j in range(28 * 28):
             image.append(ord(f.read(1)))
         input_image_data.append(image)
 
@@ -23,7 +25,64 @@ def convert_to_np(imgf, labelf, n):
     l.close()
     return input_image_data, input_label_data
 
-train_data, train_label = convert_to_np("./dataset/mnist/train-images-idx3-ubyte.gz", "./dataset/mnist/train-labels-idx1-ubyte.gz",60000)
-test_data, test_label = convert_to_np("./dataset/mnist/t10k-images-idx3-ubyte.gz", "./dataset/mnist/t10k-labels-idx1-ubyte.gz",10000)
-print(test_data)
-print(test_label)
+
+train_data, train_label = convert_to_np(
+    "./dataset/mnist/train-images-idx3-ubyte.gz",
+    "./dataset/mnist/train-labels-idx1-ubyte.gz",
+    20,
+)
+test_data, test_label = convert_to_np(
+    "./dataset/mnist/t10k-images-idx3-ubyte.gz",
+    "./dataset/mnist/t10k-labels-idx1-ubyte.gz",
+    1,
+)
+
+zipped = list(zip(train_data, train_label))
+random.shuffle(zipped)
+train_data, train_label = zip(*zipped)
+train_data, train_label = list(train_data), list(train_label)
+
+print("Dataset loaded")
+
+
+one_hot_dict = {
+    0: [1,0,0,0,0,0,0,0,0,0],
+    1: [0,1,0,0,0,0,0,0,0,0],
+    2: [0,0,1,0,0,0,0,0,0,0],
+    3: [0,0,0,1,0,0,0,0,0,0],
+    4: [0,0,0,0,1,0,0,0,0,0],
+    5: [0,0,0,0,0,1,0,0,0,0],
+    6: [0,0,0,0,0,0,1,0,0,0],
+    7: [0,0,0,0,0,0,0,1,0,0],
+    8: [0,0,0,0,0,0,0,0,1,0],
+    9: [0,0,0,0,0,0,0,0,0,1],
+}
+
+
+
+# Declare the network
+n = MLP(2, [784, 10])
+
+for _ in range(10):
+    ypred = [n(x) for x in train_data]
+    individual_loss = []
+    for ygt, yout in zip(train_label,ypred):
+        for youtt,ygtt in zip(yout,one_hot_dict[ygt]):
+            individual_loss.append((youtt-ygtt)**2)
+
+
+    loss = sum( individual_loss )
+    print(loss)
+    for p in n.parameters():
+        p.grad = 0.0
+
+    loss.backward()
+    for p in n.parameters():
+        p.data += -(0.1 * p.grad)
+
+ypred = [n(x) for x in test_data]
+for ygt, yout in zip(test_label, ypred):
+    for youtt,ygtt in zip(yout,one_hot_dict[ygt]):
+        print("Label: ",ygtt)
+        print("Predictions: ",youtt)
+
